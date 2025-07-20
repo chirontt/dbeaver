caa3634263f3d054829fe12d560dbe473e80a472
Tue Ton <49886739+chirontt@users.noreply.github.com>
2025-07-20 15:01:48 -0400

    Add linux.riscv64, win32.aarch64 & FreeBSD aarch64/x86_64 platforms' build

--- tools/build.sh
+++ tools/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 cd ..
 [ ! -d ../dbeaver-common ] && git clone https://github.com/dbeaver/dbeaver-common.git ../dbeaver-common
 [ ! -d ../dbeaver-jdbc-libsql ] && git clone https://github.com/dbeaver/dbeaver-jdbc-libsql.git ../dbeaver-jdbc-libsql
